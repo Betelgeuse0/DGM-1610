1d2ff2070e28e572d121fc7d28a9d9e44c1bc1dd
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-01 11:18:14 -0700

    Started Player Code and added a RigidBody

---
+++ DGM1610_P1/Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("up");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            Debug.Log("left");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            Debug.Log("right");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+            Debug.Log("down");
+        }
+    }
+}
