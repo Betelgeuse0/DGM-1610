0fc9a2d3a8d83f43b5430745c826ba882224c4d3
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-01 17:12:03 -0700

    Improved Player Movement
    Improved movement by increasing drag and improving code logic.

--- DGM1610_P1/Assets/Player.cs
+++ DGM1610_P1/Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public Rigidbody rb;
     public float force = 10.0f;
+    public float max_speed = 2.0f;
 
     void Start()
     {
@@ -14,27 +15,29 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        Vector3 rvel = rb.velocity;
+
+        if (rvel.x < max_speed && Input.GetKey(KeyCode.W))
         {
             rb.AddForce(new Vector3(force, 0, 0), ForceMode.Force);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (rvel.z < max_speed && Input.GetKey(KeyCode.A))
         {
             rb.AddForce(new Vector3(0, 0, force), ForceMode.Force);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (rvel.z > -max_speed && Input.GetKey(KeyCode.D))
         {
             rb.AddForce(new Vector3(0, 0, -force), ForceMode.Force);
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (rvel.x > -max_speed && Input.GetKey(KeyCode.S))
         {
             rb.AddForce(new Vector3(-force, 0, 0), ForceMode.Force);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (rvel.y < max_speed && Input.GetKey(KeyCode.Space))
         {
             rb.AddForce(new Vector3(0, force, 0), ForceMode.Force);
         }
