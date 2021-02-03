c89e131f1f2c66791fe43bf43587678a07fbbf87
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-02 23:38:43 -0700

    Camera Rotation Working
    Camera Rotation from a 1st person perspective now works.

--- DGM1610_P1/Assets/CameraLogic.cs
+++ DGM1610_P1/Assets/CameraLogic.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraLogic : MonoBehaviour
 {
     public float rotationSpeed = 10;
+    public float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +25,21 @@ public class CameraLogic : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None;
             else if (Input.GetMouseButtonDown(0)) {
                 Cursor.lockState = CursorLockMode.Locked;
-                Debug.Log(Input.mousePosition);
             }
             
+            /*timer += Time.deltaTime;
+            if (timer > 1.0f) 
+                Debug.Log(Input.mousePosition);*/
+            
+
+            float h = rotationSpeed * Input.GetAxis("Mouse X");
+            float v = rotationSpeed * Input.GetAxis("Mouse Y");
+
+            //transform.Rotate(-v, h, 0);
+
+           Vector3 rotation = transform.eulerAngles;
+            rotation.x -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+            rotation.y += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            transform.eulerAngles = rotation;
     }
 }
