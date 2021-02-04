17c5b93500335d656e6b3746f24342043dd21526
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-03 19:35:21 -0700

    Started on Jumping Logic
    todo for jumping: make sure to check the position of the object you collide with (so you can't climb walls for example).

--- DGM1610_P1/Assets/Player.cs
+++ DGM1610_P1/Assets/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody rb;
-    public float force = 10.0f;
-    public float max_speed = 2.0f;
+    public float walkForce = 10.0f;
+    public float jumpForce = 100.0f;
+    public float maxSpeed = 2.0f;
     public float rotationSpeed = 10.0f;
-
+    private bool grounded = false;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,12 +22,26 @@ public class Player : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        if (rb.velocity.magnitude < max_speed)
-            rb.AddForce(new Vector3(force * v, 0, -force * h));
+        if (rb.velocity.magnitude < maxSpeed)
+            rb.AddForce(new Vector3(walkForce * v, 0, -walkForce * h));
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) 
+        {
+            grounded = false;
+            rb.AddForce(new Vector3(0, jumpForce, 0));
+        }
         
         Vector3 rotation = transform.eulerAngles;
         //rotation.x -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
         rotation.y += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
         transform.eulerAngles = rotation;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "block")
+        {
+            grounded = true;
+        }
     }
 }
