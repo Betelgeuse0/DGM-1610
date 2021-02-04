ec4c6a8be60471b711e5b1029b0001c918baf518
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-04 11:12:40 -0700

    Dynamic Jumping
    now the jump will be cut short if the player releases the space bar while they're jumping

--- DGM1610_P1/Assets/Player.cs
+++ DGM1610_P1/Assets/Player.cs
@@ -9,7 +9,6 @@ public class Player : MonoBehaviour
     public float jumpForce = 100.0f;
     public float maxSpeed = 2.0f;
     public float rotationSpeed = 10.0f;
-    private bool grounded = false;
     
     void Start()
     {
@@ -28,15 +27,18 @@ public class Player : MonoBehaviour
 
         //jumping logic
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, (GetComponent<BoxCollider>().size.y / 2.0f) + 0.01f))
-            if (Input.GetKeyDown(KeyCode.Space))
-                rb.AddForce(new Vector3(0, jumpForce, 0));
 
-        /*if (grounded && Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector3(0, jumpForce, 0));
-            grounded = false;
-        }*/
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, (GetComponent<BoxCollider>().size.y / 2.0f) + 0.01f)) 
+            {
+                rb.AddForce(new Vector3(0, jumpForce, 0));
+            }
+        }
+        else if (!Input.GetKey(KeyCode.Space) && rb.velocity.y > 0.0f)  //stop the jump short
+        {
+            rb.AddForce(new Vector3(0, -rb.velocity.y, 0));    
+        }
         
         Vector3 rotation = transform.eulerAngles;
         //rotation.x -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
@@ -45,12 +47,5 @@ public class Player : MonoBehaviour
 
     }
 
-    /*void OnCollisionEnter(Collision coll)
-    {
-        if (coll.gameObject.tag == "block" && coll.gameObject.transform.position.y < transform.position.y)
-        {
-            grounded = true;
-        }
-    }*/
 
 }
