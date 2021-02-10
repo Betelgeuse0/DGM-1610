8ef6a4c4e90d8941f225a624725b625f13d23801
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-09 18:59:00 -0700

    Variables And Operators Activity

---
+++ DGM1610_P1/Assets/Operators.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VarsAndOps    //include variables.cs
+{
+    public class Ops
+    {
+        static public void StartHere()
+        {
+            ++Vars.myInt;
+            --Vars.myLong;
+            Vars.myString = Vars.myString.Substring(5, 6) + "changed man";
+            Vars.myBool = !Vars.myBool;
+
+            Debug.Log(Vars.myInt);
+            Debug.Log(Vars.myLong);
+            Debug.Log(Vars.myFloat);
+            Debug.Log(Vars.myDecimal);
+            Debug.Log(Vars.myString);
+            Debug.Log(Vars.myBool);
+        }
+
+    }
+}
+
+public class Operators : MonoBehaviour
+{
+    void Start()
+    {
+        VarsAndOps.Ops.StartHere();
+    }
+}
---
+++ DGM1610_P1/Assets/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VarsAndOps
+{
+    public class Vars
+    {
+        //variables
+        static public int myInt = 8008135;
+        static public double myDouble = 33.333d;
+        static public float myFloat = 1.0232f;
+        static public string myString = "I'm a string";
+        static public decimal myDecimal = 3894.0M;
+        static public long myLong = 9999999999L;
+        static public bool myBool = true;
+    }
+}
