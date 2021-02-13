9576d214b0c1fa60c5b09c25296847c1a9688798
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-12 17:40:44 -0700

    Pickaxe Swing Logic

---
+++ DGM1610_P1/Assets/PIckaxe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PIckaxe : MonoBehaviour
+{
+    private float swingTimer = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Quaternion r = transform.rotation;
+            r.x += 30;
+            transform.rotation = r;
+            swingTimer = 0.5f;
+        }
+        else 
+        {
+            if (swingTimer > 0)
+                swingTimer -= Time.deltaTime;
+            else if (transform.rotation.x != 0.0f)
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+    }
+}
