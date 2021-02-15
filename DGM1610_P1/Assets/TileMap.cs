ffa6a99f45851379449ac0cff131326096ac1cd5
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-15 15:20:52 -0700

    More work on 3d Tilemap

--- DGM1610_P1/Assets/TileMap.cs
+++ DGM1610_P1/Assets/TileMap.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class TileMap : MonoBehaviour
 {
     public int tileDim = 1; //the 3d tile dimensions 
-    public Vector3 mapDim = new Vector3(10, 10, 0);     //the width and height of the map
+    public Vector3 mapDim = new Vector3(10, 10, 1);     //the width and height of the map
     private GameObject tile;
     void Start()
     {
         tile = transform.GetChild(0).gameObject;
         tile.transform.localScale = new Vector3(tileDim, tileDim, tileDim);
 
-        for (int x = 0; x < mapDim.x; x++)
+        for (int x = 0; x < mapDim.x; x += tileDim)
         {
-            for (int y = 0; y < mapDim.x; y++)
+            for (int y = 0; y < mapDim.y; y += tileDim)
             {
-                for (int z = 0; z < mapDim.z; y++)
+                for (int z = 0; z < mapDim.z; z += tileDim)
                 {
                     //place tiles
-                    //instantiate();
+                    GameObject clone = GameObject.Instantiate(tile);
+                    clone.transform.position = new Vector3(x, y, z);
                 }
             }
         }
