830f7d2cd973c6947c77849f9577c6c1cb128781
Kenton Renshaw <kentorenshaw@gmail.com>
2021-02-19 20:17:54 -0700

    Conditionals And Switches

---
+++ DGM1610_P1/Assets/Scripts/Conditionals And Switches/Conditionals.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int x = 50;
+    public int y = 10;
+    public string containing = "Hello Bob";
+    void Start()
+    {
+        if (x > y) 
+        {
+            Debug.Log("Hello World");
+        }
+        else if (x < y)
+        {
+            Debug.Log("x < y");
+        }
+        else 
+        {
+            Debug.Log("x == y");
+        }
+
+        if (containing.Contains("Bob"))   //check if the string contains "Bob"
+        {
+            Debug.Log("Hello, Bob");
+        }
+        
+    }
+}
---
+++ DGM1610_P1/Assets/Scripts/Conditionals And Switches/Switches.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum Importance
+    {
+        Low,
+        Medium,
+        High,
+        Urgant
+    }
+    public Importance imp = Importance.High;
+    
+    void Start()
+    {
+        switch (imp)
+        {
+            case Importance.Low:
+                Debug.Log("low importance...");
+                break;
+            case Importance.Medium:
+                Debug.Log("medium importance...");
+                break;
+            case Importance.High:
+                Debug.Log("now we're getting somewhere");
+                break;
+            case Importance.Urgant:
+                Debug.Log("WE ARE REACHING MAXIMUM IMPORTANCE LEVELS!!!!");
+                break;
+            default:
+                Debug.Log("This doesn't seem very important");
+                break;
+        }
+    }
+}
