ef3b817afb87be9b6a95f33385af5bbc8850219b
Kenton Renshaw <kentorenshaw@gmail.com>
2021-04-16 19:58:48 -0600

    Events

---
+++ DGM1610_P1/Assets/Scripts/UnityEventBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParamsEvent : UnityEvent<string, int>
+{
+    
+}
+
+public class UnityEventBehaviour : MonoBehaviour
+{
+    UnityEvent myEvent;
+    ParamsEvent paramsEvent;
+    
+    void Start()
+    {
+        myEvent = new UnityEvent();
+        myEvent.AddListener(FireFunction);
+        myEvent.AddListener(AnotherFireFunction);
+
+        paramsEvent = new ParamsEvent();
+        paramsEvent.AddListener(FireFunctionWithParams);
+        
+        myEvent.Invoke();
+        paramsEvent.Invoke("hello", 22);
+        paramsEvent.Invoke("jdawgs", 8008135);
+    }
+    
+    void FireFunction()
+    {
+        Debug.Log("You fired a function");
+    }
+
+    void AnotherFireFunction()
+    {
+        Debug.Log("You fired ANOTHER function");
+    }
+
+    void FireFunctionWithParams(string str, int num)
+    {
+        Debug.Log($"The string you passed is \"{str}\" and the number you passed is \"{num}\"");
+    }
+    
+}
